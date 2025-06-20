ec7729fd4b45e861aabc7790c6ce0e388d1564a8
Daniel Adeboye <49316764+Danosky8082@users.noreply.github.com>
2025-06-20 15:46:29 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
 
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: 'class',
+  theme: { extend: {} },
+  plugins: [],
+};
+
+// PostCSS config (postcss.config.js)
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
