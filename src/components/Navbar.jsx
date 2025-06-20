f3db0d8625b8be8b13c7d17411647ef21a41bd00
Daniel Adeboye <49316764+Danosky8082@users.noreply.github.com>
2025-06-20 15:20:20 +0100

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Navbar = () => (
+  <nav className="bg-blue-600 text-white p-4 flex justify-between">
+    <div className="font-bold">Task Manager</div>
+    <div className="space-x-4">
+      <Link to="/">Home</Link>
+      <Link to="/tasks">Tasks</Link>
+    </div>
+  </nav>
+);
+export default Navbar;
